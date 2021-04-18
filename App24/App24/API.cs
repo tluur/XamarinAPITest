f67ab3df65220615f55f59054596008b29a6bc3c
tluur <tluur@tlu.ee>
2021-04-18 17:33:21 +0300

    Add project files.

---
+++ App24/App24/API.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace App24
+{
+    public class API : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Namme { get; set; }
+        public string dedcription { get; set; }
+        public List<string> dough_ingredients { get; set; }
+        public List<string> top_ingredients { get; set; }
+        public string Method { get; set; }
+        public int id { get; set; }
+
+        private string _title;
+
+        [JsonProperty("namme")]
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged(); //This notifies the View or ViewModel that the value that a property in the Model has changed and the View needs to be updated.
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ App24/App24/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace App24
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private const string Url = "https://myt-pizza-api.herokuapp.com/pizzas"; //minu  API
+        private readonly HttpClient _client = new HttpClient();
+        private ObservableCollection<API> _namme;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            string content = await _client.GetStringAsync(Url); // Teeb Get
+            List<API> pizzas = JsonConvert.DeserializeObject<List<API>>(content); 
+            _namme = new ObservableCollection<API>(pizzas);
+            MyListView.ItemsSource = _namme; 
+            base.OnAppearing();
+        }
+
+        private async void OnAdd(object sender, EventArgs e)
+        {
+            API post = new API { Title = $" Lisame aja :  {DateTime.Now}" };  // Post aja
+            string content = JsonConvert.SerializeObject(post); 
+            await _client.PostAsync(Url, new StringContent(content, Encoding.UTF8, "application/json"));
+            _namme.Insert(0, post); 
+        }
+
+        private async void OnDelete(object sender, EventArgs e)
+        {
+            API post = _namme[0];
+            await _client.DeleteAsync(Url + "/" + post.Id); //Kustutab id
+            _namme.Remove(post);
+        }
+    }
+}
